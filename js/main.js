acf22cba8dc334fc324c231c913238f220e5a4a5
mahmoud595 <mahmoud595@yahoo.com>
2020-10-08 15:04:16 -0700

    2 sliders

--- js/main.js
+++ js/main.js
@@ -1,37 +1,44 @@
 var dots = document.querySelectorAll('.column')
-let slideindex = 0
-showDot(slideindex)
-function showDot(n){
+var dots2 =document.querySelectorAll('.column2')
+var nav = document.querySelector(".nav")
+var nav2 = document.querySelector(".nav2")
+var prev = document.querySelector(".prev")
+var next = document.querySelector(".next")
+var prev2 = document.querySelector(".prev2")
+var next2 = document.querySelector(".next2")
+var slideindex = 0
+var slideindex2 = 0
+showDot(slideindex , dots)
+showDot(slideindex2 , dots2)
 
-    if(n > dots.length-1){
+function showDot(n , col){
+
+    if(n > col.length-1){
        
        n = 0
      
     }
     if(n < 0 ){
-        n =dots.length - 1
+        n =col.length - 1
        
     }
-    for(var i = 0 ; i < dots.length ; i++){
-        dots[i].className = dots[i].className.replace
+    for(var i = 0 ; i < col.length ; i++){
+        col[i].className = col[i].className.replace
         ("active" ," ")  
      
      }
     
-     dots[n].className += " active"
+     col[n].className += " active"
 }
 
 dots.forEach(function (item , index){
     item.addEventListener("click" , function(){
         slideindex = index
-        showDot(slideindex)
+        showDot(slideindex ,dots)
     })
    
 })
 
-var nav = document.querySelector(".nav")
-var prev = document.querySelector(".prev")
-var next = document.querySelector(".next")
 
 next.addEventListener("click" , function(){
     nav.scrollLeft += dots[0].offsetWidth
@@ -39,7 +46,7 @@ next.addEventListener("click" , function(){
         slideindex = -1
         nav.scrollLeft = 0
     }
-    showDot(slideindex+=1)
+    showDot(slideindex+=1 ,dots)
 
 })
 
@@ -50,7 +57,39 @@ prev.addEventListener("click" , function(){
         nav.scrollLeft = nav.scrollWidth
     }
 
-    showDot(slideindex-=1)
+    showDot(slideindex-=1 ,dots)
 
 
 })
+
+dots2.forEach(function (item , index){
+    item.addEventListener("click" , function(){
+        slideindex2 = index
+        showDot(slideindex2 ,dots2)
+    })
+   
+})
+
+
+next2.addEventListener("click" , function(){
+    nav2.scrollLeft += dots2[0].offsetWidth
+    if(slideindex2===dots2.length-1){
+        slideindex2 = -1
+        nav2.scrollLeft = 0
+    }
+    showDot(slideindex2+=1 ,dots2)
+
+})
+
+prev2.addEventListener("click" , function(){
+    nav2.scrollLeft -= dots2[0].offsetWidth
+    if(slideindex2<= 0){
+        slideindex2 = dots2.length 
+        nav2.scrollLeft = nav2.scrollWidth
+    }
+
+    showDot(slideindex2-=1 ,dots2)
+
+
+})
+
