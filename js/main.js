2bd66c10baa4a40db9e1e6df2d3d23885570c3ff
mahmoud595 <mahmoud595@yahoo.com>
2020-10-08 19:52:50 -0700

    finish with 4 sliders

--- js/main.js
+++ js/main.js
@@ -1,18 +1,38 @@
 var dots = document.querySelectorAll('.column')
 var dots2 =document.querySelectorAll('.column2')
+var dots3 =document.querySelectorAll('.column3')
+var dots4 =document.querySelectorAll('.column4')
+
 var nav = document.querySelector(".nav")
 var nav2 = document.querySelector(".nav2")
+var nav3 = document.querySelector(".nav3")
+var nav4 = document.querySelector(".nav4")
+
 var prev = document.querySelector(".prev")
 var next = document.querySelector(".next")
+
 var prev2 = document.querySelector(".prev2")
 var next2 = document.querySelector(".next2")
+
+var prev3 = document.querySelector(".prev3")
+var next3 = document.querySelector(".next3")
+
+var prev4 = document.querySelector(".prev4")
+var next4 = document.querySelector(".next4")
+
+
 var slideindex = 0
 var slideindex2 = 0
+var slideindex3 = 0
+var slideindex4 = 0
+
+
 showDot(slideindex , dots)
 showDot(slideindex2 , dots2)
+showDot(slideindex3 , dots3)
+showDot(slideindex4 , dots4)
 
-function showDot(n , col){
-
+function showDot(n , col){   
     if(n > col.length-1){
        
        n = 0
@@ -93,3 +113,71 @@ prev2.addEventListener("click" , function(){
 
 })
 
+
+
+
+dots3.forEach(function (item , index){
+    item.addEventListener("click" , function(){
+        slideindex3 = index
+        showDot(slideindex3 ,dots3)
+    })   
+})
+
+next3.addEventListener("click" , function(){
+    nav3.scrollLeft += dots3[0].offsetWidth
+    if(slideindex3===dots3.length-1){
+        slideindex3 = -1
+        nav3.scrollLeft = 0
+    }
+    showDot(slideindex3+=1 ,dots3)
+})
+
+prev3.addEventListener("click" , function(){
+    nav3.scrollLeft -= dots3[0].offsetWidth
+    if(slideindex3<= 0){
+        slideindex3 = dots3.length 
+        nav3.scrollLeft = nav3.scrollWidth
+    }
+
+    showDot(slideindex3-=1 ,dots3)
+})
+
+
+
+
+
+
+
+
+
+
+dots4.forEach(function (item , index){
+    item.addEventListener("click" , function(){
+        slideindex4 = index
+        showDot(slideindex4 ,dots4)
+    })
+   
+})
+
+
+next4.addEventListener("click" , function(){
+    nav4.scrollLeft += dots4[0].offsetWidth
+    if(slideindex4===dots4.length-1){
+        slideindex4 = -1
+        nav4.scrollLeft = 0
+    }
+    showDot(slideindex4+=1 ,dots4)
+
+})
+
+prev4.addEventListener("click" , function(){
+    nav4.scrollLeft -= dots4[0].offsetWidth
+    if(slideindex4<= 0){
+        slideindex4 = dots4.length 
+        nav4.scrollLeft = nav4.scrollWidth
+    }
+
+    showDot(slideindex4-=1 ,dots4)
+
+
+})
